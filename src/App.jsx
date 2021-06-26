c3526ecade4709d069b08eb76761c7104f1d0ada
Mauricio Suarez <mauriciosuarez@MBP-de-Mauricio.cogeco.local>
2021-06-26 13:31:24 -0400

    Creados CartWidget y ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/styles.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+const nombreTienda = "La Tienda de la Abuela Bertha";
 
 function App() {
   return (
     <>
     <div className="App">
       <header className="header">
-      <NavBar navTheme="navbar-dark bg-dark" />
+      <NavBar navTheme="navbar-dark bg-dark" logoAlt={nombreTienda}/>
+      <ItemListContainer mensajeBienvenida={`Bienvenido a ${nombreTienda}`}/>
       </header>
     </div>
     </>
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ItemListContainer = (props) => {
+    return (
+        <h1 className="text-center my-3">{props.mensajeBienvenida}</h1>
+    );
+};
+
+export default ItemListContainer;
