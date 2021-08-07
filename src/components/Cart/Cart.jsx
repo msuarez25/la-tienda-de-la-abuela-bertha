cce471644427c5de9499b2544c9c134f0fc82cc0
Mauricio Suarez <mauriciosuarez@MacBook-Pro-de-Mauricio.local>
2021-08-07 11:46:46 -0400

    form

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { Context } from "../../Context/Context";
-import CartItem from "./CartItem";
-import PriceFormat from "../PriceFormat/PriceFormat";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Context } from '../../Context/Context';
+import CartItem from './CartItem';
+import PriceFormat from '../PriceFormat/PriceFormat';
+import Form from '../Form/Form';
 
 const Cart = () => {
   const { cartObj } = useContext(Context);
@@ -16,26 +17,27 @@ const Cart = () => {
   };
 
   return (
-    <div className="container py-5">
-      <h1 className="text-center mb-3">Carrito</h1>
+    <div className='container py-5'>
+      <h1 className='text-center mb-3'>Carrito</h1>
       {cartObj.products.length > 0 ? (
         <>
-          <div className="row fw-bold text-center">
-            <div className="col"></div>
-            <div className="col">Nombre del Producto</div>
-            <div className="col">Cantidad</div>
-            <div className="col">Precio x Unidad</div>
-            <div className="col">Precio Total</div>
-            <div className="col">Acciones</div>
+          <div className='row fw-bold text-center'>
+            <div className='col'></div>
+            <div className='col'>Nombre del Producto</div>
+            <div className='col'>Cantidad</div>
+            <div className='col'>Precio x Unidad</div>
+            <div className='col'>Precio Total</div>
+            <div className='col'>Acciones</div>
           </div>
           {cartObj.products.map((product, index) => (
             <CartItem key={index} product={product} />
           ))}
-          <div className="totalCart py-5 text-center row justify-content-end fw-bold align-items-center">
-            <div className="col-2">TOTAL A PAGAR</div>
-            <div className="col-2">{totalCart(false)}</div>
-            <div className="col-2">
-              <Link
+          <div className='totalCart py-5 text-center row justify-content-end fw-bold align-items-center'>
+            <div className='col-2'>TOTAL A PAGAR</div>
+            <div className='col-2'>{totalCart(false)}</div>
+            <div className='col-2'>
+              <Form />
+              {/* <Link
                 to={{
                   pathname:
                     `https://www.paypal.com/paypalme/msuarez25/` +
@@ -44,14 +46,14 @@ const Cart = () => {
                 target="_blank"
               >
                 <button className="btn btn-warning">PAGAR</button>
-              </Link>
+              </Link> */}
             </div>
           </div>
         </>
       ) : (
-        <div className="text-center">
+        <div className='text-center'>
           <p>No hay items en el carrito</p>
-          <Link className="btn btn-dark" aria-current="page" to="/">
+          <Link className='btn btn-dark' aria-current='page' to='/'>
             Inicio
           </Link>
         </div>
